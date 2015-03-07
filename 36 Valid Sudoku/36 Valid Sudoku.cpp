b3f12141bd5e39f056e882356bbce050d8212ab9
brantzhang <hj-zhang@foxmail.com>
2015-03-07 14:45:14 +0800

    不使用map最为hash查找，使用set容器进行查找

--- 36 Valid Sudoku/36 Valid Sudoku.cpp
+++ 36 Valid Sudoku/36 Valid Sudoku.cpp
@@ -3,6 +3,8 @@ Determine if a Sudoku is valid, according to: Sudoku Puzzles - The Rules.
 
 The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
 */
+
+// 1st solution
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char> > &board) {
@@ -33,6 +35,47 @@ public:
                         if(board[i+a][j+b] != '.'){
                             if(hashtable3.find(board[i+a][j+b]) != hashtable3.end()) return false;
                             hashtable3[board[i+a][j+b]]=j;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
+};
+
+//2nd solution
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char> > &board) {
+        set<char> hashtable;
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j] != '.'){
+                    if(hashtable.find(board[i][j]) != hashtable.end()) return false;
+                    hashtable.insert(board[i][j]);
+                }
+            }
+            
+            hashtable.clear();
+            for(int j=0;j<9;j++){
+                if(board[j][i] != '.'){
+                    if(hashtable.find(board[j][i]) != hashtable.end()) return false;
+                    hashtable.insert(board[j][i]);
+                }
+            }
+            hashtable.clear();
+        }
+        
+        for(int i=0;i<9;i+=3){ 
+            for(int j=0;j<9;j+=3){
+                hashtable.clear();
+                for(int a=0;a<3;a++){
+                    for(int b=0;b<3;b++){
+                        if(board[i+a][j+b] != '.'){
+                            if(hashtable.find(board[i+a][j+b]) != hashtable.end()) return false;
+                            hashtable.insert(board[i+a][j+b]);
                         }
                     }
                 }
