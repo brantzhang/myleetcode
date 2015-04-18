3dfdbed8b70cbe473b79d2e20f4d2fcc72c2b37f
brantzhang <hj-zhang@foxmail.com>
2015-04-18 21:00:49 +0800

    201 Bitwise AND of Numbers Range

--- 201 Bitwise AND of Numbers Range.cpp
+++ 201 Bitwise AND of Numbers Range.cpp
@@ -4,7 +4,7 @@ Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of 
 For example, given the range [5, 7], you should return 4.
 Credits:
 Special thanks to @amrsaqr for adding this problem and creating all test cases.
-*/
+****************/
 //连续的数进行按位与，最终结果就是看左边的最长公共位数，连续右移，直至m==n,此时剩下的就是最长公共位数，再右移回指定位置即可
 class Solution {
 public:
